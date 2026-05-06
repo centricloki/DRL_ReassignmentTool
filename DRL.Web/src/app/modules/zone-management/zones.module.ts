082711db1d1dc572eab2ecce1888fd458373c8fe
centricloki <lokender.tiwari@centricconsulting.com>
2026-05-06 12:08:08 +0530

    Import additional modules for zone management
    Added Angular Material and Kendo UI modules for enhanced UI components.

--- DRL.Web/src/app/modules/zone-management/zones.module.ts
+++ DRL.Web/src/app/modules/zone-management/zones.module.ts
@@ -1,15 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ZonesRoutingModule } from './zones-routing.module';
 import { ZoneListComponent } from './zone-list/zone-list.component';
 import { ManageZoneComponent } from './manage-zone/manage-zone.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { MatRippleModule, MatTableModule, MatPaginatorModule, MatSortModule, MatSelectModule } from '@angular/material';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { TooltipModule } from '@progress/kendo-angular-tooltip';
+import { InputsModule } from '@progress/kendo-angular-inputs';
 
 @NgModule({
   declarations: [ZoneListComponent, ManageZoneComponent],
   imports: [
     CommonModule,
-    ZonesRoutingModule
+    ZonesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatRippleModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    GridModule,
+    DropDownsModule,
+    TooltipModule,
+    InputsModule
   ]
 })
 export class ZonesModule { }
